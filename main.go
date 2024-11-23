506a33c714a55e09a3685e852dbde974b7eab832
snowkluster <rohankaushik1@outlook.com>
2024-11-23 18:40:54 +0530

    get port from env

--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"hash-api/controller"
+	"github.com/joho/godotenv"
+	"os"
+	"log"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/public", "./public")
 	r.SetTrustedProxies([]string{"localhost"})
@@ -17,5 +28,5 @@ func main() {
 	r.POST("/upload", controller.UploadFile)
 	r.GET("/checkhash", controller.Hashpage)
 	r.POST("/checkhash", controller.Viruscheck)
-	r.Run(":8080")
+	r.Run(":" + port)
 }
